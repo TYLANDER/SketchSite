524d9ce63dcc56864d25f802d30738d082f7989f
Tyler Schmidt <schmidt197@gmail.com>
2025-06-15 16:13:17 -0700

    Implement ChatGPT API integration for HTML/CSS generation from canvas sketches

---
+++ Services/ChatGPTService.swift
@@ -0,0 +1,8 @@
+//
+//  ChatGPTService.swift
+//  SketchSite
+//
+//  Created by Tyler Schmidt on 6/15/25.
+//
+
+import Foundation
---
+++ Utilities/LayoutDescriptor.swift
@@ -0,0 +1,8 @@
+//
+//  LayoutDescriptor.swift
+//  SketchSite
+//
+//  Created by Tyler Schmidt on 6/15/25.
+//
+
+import Foundation
