187ce8ab623127bf6ca4b215f8ce0dfc9ad5c857
Tyler Schmidt <schmidt197@gmail.com>
2025-07-22 10:12:03 -0700

    Clean up after GenerateAPIKeys.swift deletion
    - Remove stale reference to deleted GenerateAPIKeys.swift from SketchSiteApp.swift
    - Regenerate Xcode project to remove file reference
    - Fix build error: 'Build input file cannot be found'
    - App now builds successfully without the temporary key generator

--- SketchSiteApp.swift
+++ SketchSiteApp.swift
@@ -5,12 +5,6 @@ import SwiftUI
 struct SketchSiteApp: App {
     init() {
         DiagnosticsRunner.runAll()
-        
-        #if DEBUG
-        // PRODUCTION API KEY GENERATOR
-        // Uncomment the line below after adding your real API keys to GenerateAPIKeys.swift
-        // GenerateAPIKeys.generateProductionKeys()
-        #endif
     }
     var body: some Scene {
         WindowGroup {
